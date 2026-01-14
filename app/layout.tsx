29f189e770e926c54c854000ef4f3b902021433d
RamRaj110 <rampawankumar110@gmail.com>
2026-01-14 17:36:58 +0530

    favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,14 @@ export const metadata: Metadata = {
   description: "A Next.js project using the App Router and Google Fonts.",
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      {
+        url: "/apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
       { url: "/favicon.ico", sizes: "any" },
     ],
     apple: [
